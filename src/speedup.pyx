b58c13e566be08403b9adfd373240e454c24618e
anders hoff <shall.be.lifted@gmail.com>
2014-07-12 23:50:05 +0200

    more tweaks. some actual speedup ...

--- src/speedup.pyx
+++ src/speedup.pyx
@@ -6,62 +6,63 @@ import numpy as np
 cimport numpy as np
 cimport cython
 
+from libc.math cimport sqrt
+from libc.math cimport pow
+
 
 DINT = np.int
 ctypedef np.int_t DINT_t
 DFLOAT = np.float
 ctypedef np.float_t DFLOAT_t
 
+@cython.cdivision(True)
+cdef inline float get_force(float lim,float a, float b):
+  cdef float dd = sqrt(pow(a,2)+pow(b,2))
+  if dd <= 0.:
+    return 0.
+  else:
+    return (lim-dd)/dd
+
 @cython.wraparound(False)
 @cython.boundscheck(False)
-def pyx_collision_reject(l,np.ndarray[DFLOAT_t,ndim=2] sx,farl):
-#def pyx_collision_reject(l,double[:,::1] sx,farl):
+@cython.nonecheck(False)
+def pyx_collision_reject(l,np.ndarray[double, mode="c",ndim=2] sx,float farl):
 
-  cdef DINT_t vnum = l.vnum
-  cdef DFLOAT_t cfarl = farl
+  cdef unsigned int vnum = l.vnum
 
-  cdef np.ndarray[DFLOAT_t,ndim=1] X = l.X[:vnum,0]
-  cdef np.ndarray[DFLOAT_t,ndim=1] Y = l.X[:vnum,1]
+  cdef np.ndarray[double, mode="c",ndim=1] X = l.X[:vnum,0].ravel()
+  cdef np.ndarray[double, mode="c",ndim=1] Y = l.X[:vnum,1].ravel()
 
-  near = l.get_all_near_vertices(cfarl)
+  near = l.get_all_near_vertices(farl)
 
-  cdef np.ndarray[DINT_t,ndim=1] n_near = np.array([len(a) for a in near],DINT)
-  cdef DINT_t ncols = np.max(n_near)
+  cdef unsigned int k
+  cdef unsigned int c
+  cdef unsigned int j
+  cdef unsigned int ind
 
-  cdef DINT_t k
-  cdef DINT_t c
-  cdef DINT_t j
-  cdef DINT_t ind
-
-  cdef DFLOAT_t x
-  cdef DFLOAT_t y
-  cdef DFLOAT_t dx
-  cdef DFLOAT_t dy
-  cdef DFLOAT_t dd
-  cdef DFLOAT_t ddsquare
-  cdef DFLOAT_t force
-  cdef DFLOAT_t resx
-  cdef DFLOAT_t resy
+  cdef float x
+  cdef float y
+  cdef float dx
+  cdef float dy
+  cdef float force
+  cdef float resx
+  cdef float resy
 
   for j in range(vnum):
-    k = n_near[j]
+    k = <unsigned int>len(near[j])
     resx = 0.
     resy = 0.
-    x = -X[j]
-    y = -Y[j]
+    x = X[j]
+    y = Y[j]
     for c in range(k):
       ind = <unsigned int>near[j][c]
       if ind == k:
         continue
-      dx = x+X[ind]
-      dy = y+Y[ind]
-      ddsquare = dx*dx+dy*dy
-      if ddsquare <= 0.:
-        continue
-      dd = ddsquare**0.5
-      force = (cfarl-dd)/dd
-      resx -= dx*force
-      resy -= dy*force
+      dx = x-X[ind]
+      dy = y-Y[ind]
+      force = get_force(farl,dx,dy)
+      resx += dx*force
+      resy += dy*force
 
     sx[j,0] += resx
     sx[j,1] += resy
